e3452fd4087da6dd7d6c038e23ff4e0cf2267abb
thiruamutha <thiruamutha1996@gmail.com>
2022-01-03 19:39:39 +0530

    B added code

--- src/org/sample/CourseDetails.java
+++ src/org/sample/CourseDetails.java
@@ -2,11 +2,15 @@ package org.sample;
 
 public class CourseDetails {
 
+	private void m2() {
+		System.out.println("Method 2 - by B");
+	}
+
 	public static void main(String[] args) {
 
 		int a = 1000;
 		System.out.println(a);
-		System.out.println(a + 100);
+
 	}
 
 }
