3885b8b722d2f3511e89329c5689997626f64407
thiruamutha <thiruamutha1996@gmail.com>
2022-01-03 19:53:28 +0530

    code added by C

--- src/org/sample/CourseDetails.java
+++ src/org/sample/CourseDetails.java
@@ -7,6 +7,9 @@ public class CourseDetails {
 		int a = 1000;
 		System.out.println(a);
 		System.out.println(a + 100);
+		
+		System.out.println("Branch C Included");
+		System.out.println("C is writing the code");
 	}
 
 }
